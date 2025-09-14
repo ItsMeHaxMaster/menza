e68c27e22c1e9c94e8e66bdf5bbcfd0256a6e071
TheClashFruit <clash.fruit123@gmail.com>
2025-09-14 10:52:31 +0200

    feat: setup api

---
+++ apps/api/src/index.ts
@@ -0,0 +1,149 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import multer from 'multer';
+import chalk from 'chalk';
+import cookies from 'cookie-parser';
+import expressWs from 'express-ws';
+
+import path from 'node:path';
+
+import { Request, Response } from '@/util/handler';
+import Logger from '@/util/logger';
+import getRoutes from '@/util/get_routes';
+import pathParser from '@/util/path_parser';
+import version from '@/util/version';
+
+import Method from '@/enum/method';
+
+const app = expressWs(express()).app;
+
+const logger = new Logger('canteen::api::main');
+const webLogger = new Logger('canteen::api::req');
+
+const PORT = parseInt(process.env.PORT!) || 3001;
+
+app.use((req, res, next) => {
+  res.setHeader('X-Powered-By', `Canteen/${version}`);
+  next();
+
+  const ip = req.headers['x-forwarded-for'] || req.ip;
+  webLogger.info(
+    `${ip} "${req.method} ${req.path} HTTP/${req.httpVersion}" ${res.statusCode} ${res.getHeader('Content-Length') ? res.getHeader('Content-Length') : '0'} "${req.get('Referer') ? req.get('Referer') : '-'}" "${req.get('User-Agent')}"`
+  );
+});
+
+app.use(
+  cors({
+    maxAge: 86400
+  })
+);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(multer().any());
+app.use(cookies());
+
+(async () => {
+  const startTimestamp = Date.now();
+
+  const routes: string[] = await getRoutes(path.join(__dirname, 'routes'));
+
+  logger.info('○ Routes:', routes.length);
+  for (const route of routes) {
+    const routePath = pathParser(
+      route.replace(__dirname, '').replace(/\\/g, '/')
+    );
+    const routeHandler = await import(route);
+
+    const methods = Object.keys(routeHandler)
+      .filter((key) => key !== 'default')
+      .map((m) => {
+        if (m === 'get') return Method.Get;
+        if (m === 'post') return Method.Post;
+        if (m === 'put') return Method.Put;
+        if (m === 'del') return Method.Delete;
+        if (m === 'patch') return Method.Patch;
+        if (m === 'options') return Method.Options;
+        if (m === 'head') return Method.Head;
+        if (m === 'ws') return Method.WebSocket;
+        else return Method.Get;
+      });
+    const isValid = methods.length > 0;
+
+    if (isValid) {
+      logger.info(
+        chalk.green('◆'),
+        `\`${routePath}\``,
+        '(' + methods.join(', ') + ')'
+      );
+
+      if (methods.includes(Method.WebSocket)) {
+        app.ws(routePath, (ws, req) => {
+          const request = new Request(req, undefined);
+          routeHandler.ws(ws, request);
+        });
+      }
+
+      app.all(routePath, (req, res) => {
+        try {
+          const response = new Response(res, req);
+          const request = new Request(req, response);
+
+          if (!response.allow(methods)) return;
+          switch (req.method) {
+            case Method.Get:
+              return routeHandler.get(request, response);
+            case Method.Post:
+              return routeHandler.post(request, response);
+            case Method.Put:
+              return routeHandler.put(request, response);
+            case Method.Delete:
+              return routeHandler.del(request, response);
+            case Method.Patch:
+              return routeHandler.patch(request, response);
+            case Method.Options:
+              return routeHandler.options(request, response);
+            case Method.Head:
+              return routeHandler.head(request, response);
+            default:
+              return routeHandler.get(request, response);
+          }
+        } catch (err: any) {
+          webLogger.error(err.message);
+
+          res.status(500).json({
+            error: 500,
+            message: 'Internal Server Error'
+          });
+        }
+      });
+    } else logger.info(chalk.red('◇'), `\`${routePath}\``);
+  }
+
+  app.use((req, res) => {
+    res.status(404).json({
+      error: 404,
+      message: 'Not Found'
+    });
+  });
+
+  app.listen(PORT, () => {
+    logger.info('○ Port:', PORT);
+    logger.info('○ Startup:', Date.now() - startTimestamp, '(ms)');
+  });
+})().catch((err) => {
+  if (!err['errors']) logger.error(err);
+  else
+    err['errors'].forEach((e: any) => {
+      logger.error(e);
+    });
+});
+
+// :3
+(Date.prototype as any).toJSON = function () {
+  return this.getTime();
+};
+
+(BigInt.prototype as any).toJSON = function () {
+  return this.toString();
+};
---
+++ apps/api/src/util/handler.ts
@@ -0,0 +1,224 @@
+import Status from '@/enum/status';
+import Method from '@/enum/method';
+
+import { CookieOptions, RequestFile } from '@/interfaces';
+
+/**
+ * Request class to handle incoming requests.
+ *
+ * @class Request
+ */
+export class Request {
+  public method: Method;
+  public headers: { [key: string]: string };
+  public query: { [key: string]: string | number };
+  public body: { [key: string]: any };
+  public files: RequestFile[];
+  public params: { [key: string]: string | number };
+  public cookies: { [key: string]: string | number };
+  public ip: string;
+
+  /**
+   * Implement a method instead of using this!
+   * This is only public for testing purposes.
+   */
+  public req: any;
+
+  private res: Response<unknown> | undefined;
+
+  constructor(req: any, res: Response<unknown> | undefined) {
+    this.method = req.method;
+    this.headers = req.headers;
+
+    this.query = req.query;
+    this.body = req.body;
+    this.files = req.files;
+    this.params = req.params;
+    this.cookies = req.cookies;
+
+    this.ip = req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.ip;
+
+    this.req = req;
+    this.res = res;
+  }
+
+  /**
+   * Get a header from the request.
+   * 
+   * @param key The header's name.
+   * 
+   * @returns The header's value.
+   */
+  public getHeader(key: string): string | undefined {
+    return this.headers[key.toLowerCase()];
+  }
+
+  /**
+   * Get a route parameter from the request.
+   *
+   * @param key The parameter's name.
+   * 
+   * @returns The parameter's value.
+   */
+  public getParam(key: string): string | number | undefined {
+    return this.params[key.toLowerCase()];
+  }
+
+  /**
+   * Get a query parameter from the request.
+   *
+   * @param key The parameter's name.
+   * 
+   * @returns The parameter's value.
+   */
+  public getQuery(key: string): string | number | undefined {
+    return this.query[key.toLowerCase()];
+  }
+
+  /**
+   * Get a cookie from the request.
+   *
+   * @param key The cookie's name.
+   * 
+   * @returns The cookie's value.
+   */
+  public getCookie(key: string): string | number | undefined {
+    return this.cookies[key.toLowerCase()];
+  }
+}
+
+/**
+ * Response class to handle outgoing responses.
+ *
+ * @class Response
+ * @template T The type of the response data.
+ */
+export class Response<T> {
+  /**
+   * Implement a method instead of using this!
+   * This is only public for testing purposes.
+   */
+  public res: any;
+
+  private req: any;
+
+  constructor(res: any, req: any) {
+    this.res = res;
+
+    this.req = req;
+  }
+
+  /**
+   * Check if the request method is allowed.
+   *
+   * @param methods The allowed methods.
+   * @returns True if the method is allowed, false otherwise.
+   * @example
+   * ```ts
+   * if (!res.allow([Method.Get, Method.Post])) return;
+   * ```
+   *
+   * @deprecated Use the appropriate named exports.
+   */
+  public allow(methods: Method[]): boolean {
+    if (!methods.includes(this.req.method)) {
+      this.res.set('Allow', methods.join(', ').toUpperCase());
+      this.error(
+        Status.MethodNotAllowed,
+        `Only ${methods.join(', ')} method(s) are allowed.)`
+      );
+
+      return false;
+    }
+
+    return true;
+  }
+
+  /**
+   * Create a cookie.
+   *
+   * @param key The name of the cookie.
+   * @param value The value of the cookie.
+   * @param options The options for the cookie.
+   */
+  public cookie(key: string, value: string, options?: CookieOptions) {
+    this.res.cookie(key.toLowerCase(), value, options);
+  }
+
+  /**
+   * Redirect the request to a different URL.
+   *
+   * @param url The URL to redirect to.
+   * @param code The status code to use for the redirect.
+   */
+  public redirect(url: string, code: Status = Status.Found) {
+    this.res.redirect(code, url);
+  }
+
+  /**
+   * Set the status code of the response.
+   *
+   * @param code The status code.
+   * @returns An object with methods to send the response.
+   * @example
+   * ```ts
+   * res.status(Status.Ok).json({ message: 'Hello World' });
+   * ```
+   */
+  public status(code: Status) {
+    return {
+      /**
+       * A JSON response.
+       *
+       * @param data The data to send.
+       */
+      json: (data?: T) => {
+        this.res.status(code).json(data);
+      },
+      /**
+       * A text response.
+       *
+       * @param data The data to send.
+       */
+      send: (data?: T) => {
+        this.res.type('txt').status(code).send(data);
+      },
+      /**
+       * Set the content type of the response.
+       *
+       * @param type The content type.
+       */
+      type: (type: string) => {
+        return {
+          /**
+           * Send the response.
+           *
+           * @param data The data to send.
+           */
+          send: (data?: T) => {
+            this.res.type(type).status(code).send(data);
+          }
+        };
+      },
+      /**
+       * Send an empty response.
+       */
+      end: () => {
+        this.res.status(code).end();
+      }
+    };
+  }
+
+  /**
+   * Send an error response.
+   *
+   * @param code The status code.
+   * @param message The error message.
+   */
+  public error(code: Status, message: string) {
+    return this.res.status(code).json({
+      error: code,
+      message
+    });
+  }
+}
