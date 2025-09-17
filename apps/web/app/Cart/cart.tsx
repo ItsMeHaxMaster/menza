068530789559b279440614063fabdf844d78ddb4
herben <heredibenedek@gmail.com>
2025-09-17 17:14:59 +0200

    cart-basics

---
+++ apps/web/app/Cart/cart.tsx
@@ -0,0 +1,16 @@
+//import styles from "./globals.css";
+
+export default function Cart() {
+  return (
+    <main>
+        <h1>Kosár és fizetés</h1>
+        <div className="cart-elements">
+            //kosár elemei
+        </div>
+        <div className="cart-buttons">
+            <button className="btn btn-blue">Fizetés</button>
+            <button className="btn btn-black">Vissza a menühöz</button>
+        </div>
+    </main>
+  );
+}
