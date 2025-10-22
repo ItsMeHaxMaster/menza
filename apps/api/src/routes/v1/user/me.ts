dee5fab06e68baae00852dd0723a0527f7c59f90
TheClashFruit <clash.fruit123@gmail.com>
2025-10-22 15:15:02 +0200

    feat: updated at, schema

--- apps/api/src/routes/v1/user/me.ts
+++ apps/api/src/routes/v1/user/me.ts
@@ -1,7 +1,25 @@
 import { Request, Response } from '@/util/handler';
+
 import Status from '@/enum/status';
 
-export const get = async (req: Request, res: Response<any>) => {
+import { z } from 'zod';
+
+export const schemas = {
+  get: {
+    res: z.object({
+      id: z.string(),
+      name: z.string(),
+      email: z.string().email(),
+      createdAt: z.date(),
+      updatedAt: z.date()
+    })
+  }
+};
+
+export const get = async (
+  req: Request,
+  res: Response<z.infer<typeof schemas.get.res>>
+) => {
   try {
     const user = await req.getUser();
 
@@ -9,7 +27,8 @@ export const get = async (req: Request, res: Response<any>) => {
       id: user.id.toString(),
       name: user.name,
       email: user.email,
-      createdAt: user.createdAt
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt
     });
   } catch (err) {
     return res.error(Status.Unauthorized, 'Unauthorized');
