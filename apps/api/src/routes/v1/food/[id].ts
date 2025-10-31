27495458b75c0ec0e8ffc03b7d99ffe7b0a98060
TheClashFruit <clash.fruit123@gmail.com>
2025-10-31 19:32:31 +0100

    feat: edit food

--- apps/api/src/routes/v1/food/[id].ts
+++ apps/api/src/routes/v1/food/[id].ts
@@ -1,3 +1,4 @@
+import { Allergen } from '@/entities/allergen.entity';
 import { Food } from '@/entities/food.entity';
 import Status from '@/enum/status';
 import { Request, Response } from '@/util/handler';
@@ -24,6 +25,19 @@ export const schemas = {
       ),
       createdAt: z.date(),
       updatedAt: z.date()
+    })
+  },
+  patch: {
+    req: z.object({
+      name: z.string(),
+      description: z.string(),
+      price: z.number(),
+      allergens: z
+        .array(z.string().transform((id: string) => BigInt(id)))
+        .optional()
+    }),
+    res: z.object({
+      message: z.string()
     })
   },
   del: {
@@ -59,6 +73,53 @@ export const get = async (
       allergens: food.allergens.getItems(),
       createdAt: food.createdAt,
       updatedAt: food.updatedAt
+    });
+  } catch (e: unknown) {
+    console.error(e);
+
+    res.error(Status.InternalServerError, 'Internal Server Error');
+  }
+};
+
+export const patch = async (
+  req: Request,
+  res: Response<z.infer<typeof schemas.patch.res>>
+) => {
+  const db = (await orm).em.fork();
+
+  try {
+    const { id } = req.params;
+    const { name, description, price, allergens } = req.validateBody(
+      schemas.patch.req
+    );
+
+    const food = await db.findOne(Food, {
+      id
+    });
+
+    if (!food) return res.error(Status.NotFound, 'Food not found.');
+
+    food.name = name;
+    food.description = description;
+    food.price = price;
+
+    food.allergens.removeAll();
+    if (allergens && allergens.length > 0) {
+      for (const allergenData of allergens) {
+        const allergen = await db.findOne(Allergen, {
+          id: BigInt(allergenData)
+        });
+
+        if (allergen) {
+          food.allergens.add(allergen);
+        }
+      }
+    }
+
+    await db.persistAndFlush(food);
+
+    res.status(Status.Ok).json({
+      message: 'Food updated successfully'
     });
   } catch (e: unknown) {
     console.error(e);
